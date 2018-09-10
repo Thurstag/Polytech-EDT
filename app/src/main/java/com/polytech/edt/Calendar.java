1a9ae28fcab08bc40cc26feb450596542724c059
Thurstag <sashaberthout@hotmail.com>
2018-09-10 20:33:31 +0200

    Create a theme for Toolbar
    Refactor navigation drawer items

--- app/src/main/java/com/polytech/edt/Calendar.java
+++ app/src/main/java/com/polytech/edt/Calendar.java
@@ -132,25 +132,7 @@ public class Calendar extends AppCompatActivity implements NavigationView.OnNavi
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        }
-        else if (id == R.id.nav_gallery) {
-
-        }
-        else if (id == R.id.nav_slideshow) {
-
-        }
-        else if (id == R.id.nav_manage) {
-
-        }
-        else if (id == R.id.nav_share) {
-
-        }
-        else if (id == R.id.nav_send) {
+        switch (item.getItemId()) {
 
         }
 
