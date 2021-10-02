e5c19dd7290d041d03ac03d27e7f1f29ca584ed7
Thurstag <sashaberthout@hotmail.com>
2021-10-02 20:10:54 +0200

    [CHG] Updates resources
    Filter resources to use based on their categories

--- app/src/main/java/com/polytech/edt/tools/Tools.java
+++ app/src/main/java/com/polytech/edt/tools/Tools.java
@@ -13,7 +13,11 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import edu.emory.mathcs.backport.java.util.Arrays;
 
 /**
  * Set of tools for developers
@@ -27,10 +31,13 @@ class Tools {
      * Tool to get the list of resources to store in assets/resources
      */
     static class ResourcesFetcher {
+        private static final Set<String> ALLOWED_CATEGORIES = new HashSet<>(Arrays.asList(new String[]{"trainee", "instructor", "classroom", "category5"}));
+
         public static void main(String[] args) throws Exception {
             // Get resources
             Document resourcesDoc = ADEResource.fetchResources();
             NodeList resourceTags = resourcesDoc.getFirstChild().getChildNodes();
+            Set<String> skippedCategories = new HashSet<>();
 
             // Gather resources
             List<Integer> resourceIds = new ArrayList<>();
@@ -38,8 +45,14 @@ class Tools {
                 Node resourceTag = resourceTags.item(i);
                 NamedNodeMap attributes = resourceTag.getAttributes();
 
-                // Skip resources without id attribute
-                if (attributes == null || attributes.getNamedItem("id") == null) {
+                if (attributes == null) {   // Skip resources without attributes
+                    continue;
+                }
+                else if (attributes.getNamedItem("id") == null) {   // Skip resources without id attribute
+                    continue;
+                }
+                else if (!ALLOWED_CATEGORIES.contains(attributes.getNamedItem("category").getNodeValue())) {    // Skip resources without allowed categories
+                    skippedCategories.add(attributes.getNamedItem("category").getNodeValue());
                     continue;
                 }
 
@@ -47,6 +60,7 @@ class Tools {
             }
 
             // Print resources
+            System.out.println("Skipped categories: " + Arrays.toString(skippedCategories.toArray()));
             System.out.println(resourceIds.size() + " resource(s) found:");
             for (int i = 0; i < resourceIds.size(); i++) {
                 System.out.print(resourceIds.get(i));
